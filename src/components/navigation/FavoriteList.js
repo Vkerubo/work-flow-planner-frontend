27d8f92390fa9248ef26bbf5c9ad7538922e63b2
Valentine <kerubovalentine1@gmail.com>
2023-06-08 15:06:05 +0300

    FavoriteList.js: import Collapse

--- src/components/navigation/FavoriteList.js
+++ src/components/navigation/FavoriteList.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import List from "@mui/material/List";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material";
+import ListItemText from "@mui/material";
+import { ListItemButton } from "@mui/material";
+import Collapse from "@mui/material";
