5a34eca3348aa910f537a605c31db263fb4a7a91
Valentine <kerubovalentine1@gmail.com>
2023-06-08 15:46:54 +0300

    Logo.js: close SVG

--- src/components/navigation/Logo.js
+++ src/components/navigation/Logo.js
@@ -12,7 +12,7 @@ export const Logo = () => {
         data-name="Layer 1"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 881.25 881.25"
-      ></svg>
+      >
 
       <g id="surface1">
         {/* Outer shape */}
