b41861c141165f25ba3fdf860649417a65935d57
Valentine <kerubovalentine1@gmail.com>
2023-06-08 11:44:05 +0300

    Projectlist.js: handleClick

--- src/components/navigation/Projectlist.js
+++ src/components/navigation/Projectlist.js
@@ -3,5 +3,9 @@ import React from "react";
 export const Projectlist = ({ list }) => {
   const [open, setOpen] = React.useState(true);
 
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return <div>Projectlist</div>;
 };
