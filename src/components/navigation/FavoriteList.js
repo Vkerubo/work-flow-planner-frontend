2a1602200909d10e6a642517389e8e7dd524bcd7
Valentine <kerubovalentine1@gmail.com>
2023-06-08 15:07:30 +0300

    FavoriteList.js: import ExpandMore

--- src/components/navigation/FavoriteList.js
+++ src/components/navigation/FavoriteList.js
@@ -6,6 +6,6 @@ import ListItemText from "@mui/material";
 import { ListItemButton } from "@mui/material";
 import Collapse from "@mui/material";
 import { ExpandLess } from "@mui/icons-material";
+import { ExpandMore } from "@mui/icons-material";
 
-import ExpandMore from "@mui/icons-material/ExpandMore";
 import Star from "@mui/icons-material/Star";
