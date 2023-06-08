889ed02672853234817e35c5fc3c09efa6b8d2ca
Valentine <kerubovalentine1@gmail.com>
2023-06-08 16:09:23 +0300

    SideNav.js: import Link from react-router-dom

--- src/components/navigation/SideNav.js
+++ src/components/navigation/SideNav.js
@@ -3,6 +3,7 @@ import "../../css/sidenav.css";
 import { Projectlist } from "./Projectlist";
 import { FavoriteList } from "./FavoriteList";
 import { Logo } from "./Logo";
+import { Link } from "react-router-dom";
 
 import { Link } from "react-router-dom";
 import { Typography, Toolbar, Divider } from "@mui/material";
