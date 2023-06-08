4f40e2245d0236e3f81e53cdcf1ce158ddac9b61
Valentine <kerubovalentine1@gmail.com>
2023-06-08 15:44:31 +0300

    Logo.js: SVG

--- src/components/navigation/Logo.js
+++ src/components/navigation/Logo.js
@@ -6,6 +6,13 @@ export const Logo = () => {
     <div>
       {/* Logo container */}
       <span class="logo"></span>
+      {/* SVG */}
+      <svg
+        id="Layer_1"
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 881.25 881.25"
+      ></svg>
     </div>
   );
 };
