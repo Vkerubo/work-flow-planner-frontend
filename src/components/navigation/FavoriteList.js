81683a83625612d46ef3b3babf0787b249760d65
Valentine <kerubovalentine1@gmail.com>
2023-06-08 15:05:07 +0300

    FavoriteList.js: import ListItemText

--- src/components/navigation/FavoriteList.js
+++ src/components/navigation/FavoriteList.js
@@ -2,3 +2,4 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import List from "@mui/material/List";
 import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
