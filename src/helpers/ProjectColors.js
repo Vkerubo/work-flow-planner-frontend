a2de7090726d1140bad98114b6b4e8f8493154da
Valentine <kerubovalentine1@gmail.com>
2023-06-09 16:51:20 +0300

    return default color object when project color is not found

--- src/helpers/ProjectColors.js
+++ src/helpers/ProjectColors.js
@@ -1,5 +1,16 @@
 import { palette } from "../theme/palette";
 
 export const ProjectColors = (project) => {
-  return palette.find((c) => c.mainColor === project.color);
+  const color = palette.find((c) => c.mainColor === project.color);
+  if (color) {
+    return color;
+  } else {
+    // Return a default color object when the project color is not found
+    return {
+      mainColor: "#000000",
+      colorLight: "#ffffff",
+      color: "#000000",
+      colorDark: "#000000",
+    };
+  }
 };
