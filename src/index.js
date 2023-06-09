17ed1721e7b2480b83b5a9498648d60933f0e2f1
Valentine <kerubovalentine1@gmail.com>
2023-06-09 08:43:55 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from "react";
+import ReactDOM from "react-dom";
 import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
-import "./index.css";
 import App from "./App";
+import "./css/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <CssBaseline />
-
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+ReactDOM.render(
+  <React.Fragment>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
 );
