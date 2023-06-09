1cecaa79e15a40f64e1a1215a701f1745fac0556
Valentine <kerubovalentine1@gmail.com>
2023-06-09 12:39:14 +0300

    Projects component to render a list of projects

---
+++ src/containers/Projects.js
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { Typography, Grid, IconButton, Tooltip, Box } from "@mui/material";
+import ControlPointIcon from "@mui/icons-material/ControlPoint";
+import Skeleton from "@mui/material/Skeleton";
+import Project from "../components/navigation/project/Project";
+import ProjectModal from "../components/navigation/modal/ProjectModal";
+
+const Projects = ({
+  projects,
+  mode,
+  postProjects,
+  handleUpdatingProject,
+  handleDeleteProject,
+}) => {
+  //handle edit project modal
+  const [openModal, setOpenModal] = React.useState(false);
+
+  // Function to open the project creation modal
+  const handleOpenModel = () => setOpenModal(true);
+  const handleCloseModel = () => setOpenModal(false);
+
+  return (
+    <>
+      {/* Heading and create new project button */}
+      <Grid container alignContent="center" justifyContent="space-between">
+        <Typography variant="h2" gutterBottom>
+          Projects
+        </Typography>
+        <Box className="flex">
+          {/* Tooltip and button to create a new project */}
+          <Tooltip title="Create New Project">
+            <IconButton
+              size="large"
+              aria-label="create project"
+              color="inherit"
+              onClick={handleOpenModel}
+            >
+              <ControlPointIcon fontSize="large" />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Grid>
+      {/* Projects grid */}
+      <Grid container spacing={2}>
+        {/* Check if there are projects */}
+        {projects.length !== 0
+          ? projects.map((project) => (
+              // Render project cards
+              <Project
+                mode={mode}
+                project={project}
+                key={`projectcard-${project.id}`}
+                handleUpdatingProject={handleUpdatingProject}
+                handleDeleteProject={handleDeleteProject}
+              />
+            ))
+          : [1, 2, 3].map((load) => {
+              return (
+                <Grid item xs={12} sm={12} md={6} lg={4} key={`load-${load}`}>
+                  {/* Skeleton loader for project card */}
+                  <Skeleton />
+                  <Skeleton
+                    sx={{ height: 100 }}
+                    animation="wave"
+                    variant="rectangular"
+                  />
+                </Grid>
+              );
+            })}
+
+        {/* create new project modal */}
+        <ProjectModal
+          project={null}
+          openModal={openModal}
+          handleCloseModel={handleCloseModel}
+          postProjects={postProjects}
+          mode={mode}
+        />
+      </Grid>
+    </>
+  );
+};
+
+export default Projects;
