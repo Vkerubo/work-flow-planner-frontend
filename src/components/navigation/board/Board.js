84e6e28ee17c2dd025590093ad02d2a8edf265e6
Valentine <kerubovalentine1@gmail.com>
2023-06-09 07:45:42 +0300

    import styles from Material UI

---
+++ src/components/navigation/board/Board.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import DropdownMenu from "../DropdownMenu";
+import BoardModal from "../modal/BoardModal";
+import TaskModal from "../modal/TaskModal";
+import Tasks from "../../containers/Tasks";
+import {
+  Grid,
+  Typography,
+  Box,
+  Tooltip,
+  IconButton,
+  Button,
+} from "@mui/material";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import AddIcon from "@mui/icons-material/Add";
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <CssBaseline />
+
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
