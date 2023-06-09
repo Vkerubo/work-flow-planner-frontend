8953c742207b58a1938bf3bd055fe9c68a80f8f0
Valentine <kerubovalentine1@gmail.com>
2023-06-09 08:04:09 +0300

    render TextField component

--- src/components/navigation/modal/TitleField.js
+++ src/components/navigation/modal/TitleField.js
@@ -1,2 +1,19 @@
 import React from "react";
 import TextField from "@mui/material";
+
+const TitleField = ({ title, setTitle, labelName }) => {
+  return (
+    <TextField
+      onChange={(e) => setTitle(e.target.value)} //updates title state when input value changes
+      value={title}
+      label={labelName}
+      variant="outlined" //applies an outlined style to the TextField.
+      color="secondary"
+      fullWidth //makes the TextField occupy the full width of its container.
+    />
+  );
+};
+
+export default TitleField;
+
+//this component provides an input field for the title
