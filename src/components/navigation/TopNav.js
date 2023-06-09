6e43f675b556e0465819ed56173fb65fa904fec2
Valentine <kerubovalentine1@gmail.com>
2023-06-09 07:31:11 +0300

    TopNav.js: import styles from MUI

--- src/components/navigation/TopNav.js
+++ src/components/navigation/TopNav.js
@@ -1,3 +1,15 @@
 import * as React from "react";
 import "../../css/topnav.css";
 import { Logo } from "./Logo";
+import MuiAppBar from "@mui/material/AppBar";
+import { Toolbar } from "@mui/material";
+import Typography from "@mui/material";
+import Badge from "@mui/material";
+import Avatar from "@mui/material";
+import TextField from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { NotificationAddOutlined } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
+import Brightness2Icon from "@mui/icons-material/Brightness2";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
